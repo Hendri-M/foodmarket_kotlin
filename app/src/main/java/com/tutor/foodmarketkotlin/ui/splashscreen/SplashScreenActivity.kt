56224524cfd5ed24d38780c327ecb08e5f0cb207
Hendri-M <hendrimuji31@gmail.com>
2024-02-06 20:45:06 +0700

    to develop with added splash screen and themes or style for splash text

---
+++ app/src/main/java/com/tutor/foodmarketkotlin/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.tutor.foodmarketkotlin.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.tutor.foodmarketkotlin.MainActivity
+import com.tutor.foodmarketkotlin.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+}
